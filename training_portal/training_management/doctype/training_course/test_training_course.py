7aad531fa361435aa58d4dadce9d1d32e1ba1cee
Resham <reshamofficial123@gmail.com>
2026-06-06 10:26:40 +0100

    Added module

---
+++ training_portal/training_management/doctype/training_course/test_training_course.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, C&S Electric and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTrainingCourse(FrappeTestCase):
+	pass
