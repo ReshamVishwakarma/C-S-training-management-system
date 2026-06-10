714a327b2c28e7a659865ad2cdf84e99558722cc
Resham <reshamofficial123@gmail.com>
2026-06-10 11:50:34 +0100

    Add attendance auto population and summary calculation

--- training_portal/training_portal/doctype/training_attendance/training_attendance.py
+++ training_portal/training_portal/doctype/training_attendance/training_attendance.py
@@ -30,3 +30,20 @@ def fetch_enrollments(training_session):
         })
 
     return rows
+
+def calculate_attendance_summary(self):
+
+    present = 0
+    absent = 0
+
+    for row in self.attendance_details:
+
+        if row.attendance_status in ["Present", "Late"]:
+            present += 1
+
+        elif row.attendance_status == "Absent":
+            absent += 1
+
+    self.present_count = present
+    self.absent_count = absent
+    self.total_participants = len(self.attendance_details)
