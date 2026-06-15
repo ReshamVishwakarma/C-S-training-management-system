efff48909f776d636d1ca376f968d62f4088c5b5
Sandhu2702 <sk0581822@gmail.com>
2026-06-15 10:48:36 +0000

    Add HR dashboard

--- training_portal/www/hr_dashboard/index.py
+++ training_portal/www/hr_dashboard/index.py
@@ -1,12 +1,39 @@
+# import frappe
+
+# def get_context(context):
+
+#     if "HR Administrator" not in frappe.get_roles():
+#         frappe.throw("Not Permitted")
+        
+#     context.user = frappe.session.user
+
+#     context.total_employees = frappe.db.count("Employee")
+#     context.total_trainers = frappe.db.count("Trainer")
+#     context.total_courses = frappe.db.count("Training Course")
+
 import frappe
 
 def get_context(context):
 
     if "HR Administrator" not in frappe.get_roles():
         frappe.throw("Not Permitted")
-        
+
     context.user = frappe.session.user
 
     context.total_employees = frappe.db.count("Employee")
     context.total_trainers = frappe.db.count("Trainer")
     context.total_courses = frappe.db.count("Training Course")
+    context.total_sessions = frappe.db.count("Training Session")
+    context.total_departments = frappe.db.count("Department")
+
+    context.recent_sessions = frappe.get_all(
+    "Training Session",
+    fields=[
+        "name",
+        "training_name",
+        "training_date",
+        "status"
+    ],
+    order_by="creation desc",
+    limit=5
+   )
